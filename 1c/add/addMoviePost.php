a1357ee2ea3887319b14b4c39e3003703e404882
dxtania <rawrtan@gmail.com>
2015-10-20 21:21:40 -0700

    Clean up addMoviePost with docs too

--- 1c/add/addMoviePost.php
+++ 1c/add/addMoviePost.php
@@ -1,27 +1,46 @@
 <?php
-
-$title = $_POST['title'];
-$year = $_POST['year'];
-$rating = $_POST['rating']; // TODO: not null
-$company = $_POST['company'];
-$genres = isset($_POST['genres']) ? $_POST['genres'] : array();
-
 $mysqli = new mysqli("localhost", "cs143", "", "CS143");
-$id = 5002; // TODO: Get from MaxMovieId table
 
-$stmt = $mysqli->prepare("INSERT INTO Movie(id, title, year, rating, company) VALUES($id, ?, ?, ?, ?)");
-$stmt->bind_param("siss", $title, $year, $rating, $company);
-
-if (!$stmt->execute()) {
-  echo "Failure";
-} else {
-  echo "$title was added!";
-}
-
-foreach ($genres as $genre) {
-  $stmt = $mysqli->prepare("INSERT INTO MovieGenre(mid, genre) VALUES($id, ?)");
-  $stmt->bind_param("s", $genre);
-  $stmt->execute();
-}
+add_movie($mysqli,
+  $_POST['title'],
+  $_POST['year'],
+  $_POST['rating'],
+  $_POST['company'],
+  isset($_POST['genres']) ? $_POST['genres'] : array());
 
 $mysqli->close();
+
+/**
+ * Add move to Movie table
+ * @param $mysqli mysqli object
+ */
+function add_movie($mysqli, $title, $year, $rating, $company, $genres) {
+  $id = get_next_movie_id($mysqli); // TODO: Get from MaxMovieId table
+
+  $stmt = $mysqli->prepare("INSERT INTO Movie(id, title, year, rating, company) VALUES($id, ?, ?, ?, ?)");
+  $stmt->bind_param("siss", $title, $year, $rating, $company);
+
+  if (!$stmt->execute()) {
+    echo "Failure";
+  } else {
+    echo "$title was added!";
+  }
+
+  foreach ($genres as $genre) {
+    $stmt = $mysqli->prepare("INSERT INTO MovieGenre(mid, genre) VALUES($id, ?)");
+    $stmt->bind_param("s", $genre);
+    $stmt->execute();
+  }
+}
+
+/**
+ * @param $mysqli mysqli object
+ * @return integer id of next movie
+ */
+function get_next_movie_id($mysqli) {
+  $result = $mysqli->query("SELECT id FROM MaxMovieID");
+  $id = $result->fetch_assoc()['id'] + 1;
+  $stmt = $mysqli->prepare("UPDATE MaxMovieID SET id=$id");
+  $stmt->execute();
+  return $id;
+}
