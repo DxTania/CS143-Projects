c0a148163fc7dc31f677c83f803d522cd6e3b2b8
dxtania <rawrtan@gmail.com>
2015-11-12 15:39:48 -0800

    Remove compiled files

--- 2/bruinbase/main.cc
+++ 2/bruinbase/main.cc
@@ -7,43 +7,14 @@
  * @date 3/24/2008
  */
 
-#include <iostream>
 #include "Bruinbase.h"
 #include "SqlEngine.h"
-#include "BTreeNode.h"
-//#include <cstdio>
-//#include <iostream>
+#include <cstdio>
 
 int main()
 {
   // run the SQL engine taking user commands from standard input (console).
-//  SqlEngine::run(stdin);
-
-  BTNonLeafNode root;
-  root.initializeRoot(0, 20, 2);
-  root.insert(30, 3);
-  root.insert(50, 5);
-  root.insert(40, 4);
-  root.insert(10, 1);
-  root.printNode();
-
-
-  RecordId id;
-  id.pid = 1;
-  id.sid = 10;
-  BTLeafNode leaf;
-
-  leaf.insert(40, id);
-  leaf.insert(10, id);
-  leaf.insert(30, id);
-
-  int i;
-  BTLeafNode sibling;
-  leaf.insertAndSplit(49, id, sibling, i);
-
-//  leaf.printNode();
-  std::cout << "--" << std::endl;
-//  sibling.printNode();
+  SqlEngine::run(stdin);
 
   return 0;
 }
